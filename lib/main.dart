68887d09f77c80030f975e00d1dc6c49bdcde3cb
Satyam Saw <satyamkumarsaw30@gmail.com>
2024-01-24 18:43:38 +0530

    Added Simple Animation - used animated position widget to animate the icon - used Future Delay method to execute logic after some delay

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:gigglegram/screens/home_screen.dart';
+import 'package:gigglegram/screens/auth/login_screen.dart';
+
+
+
+late Size mq;
 
 void main() {
   runApp(const MyApp());
@@ -29,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:gigglegram/screens/home_screen.dart';
+
+import '../../main.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      _isAnimate = true;
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    mq = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Welcome to Giggle Gram"),
+      ),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            top: mq.height * 0.15,
+            left: _isAnimate ? mq.width * 0.25 : -mq.width * 0.5,
+            width: mq.width * 0.5,
+            child: Image.asset(
+              'assets/images/icon_google.png',
+            ),
+          ),
+          Positioned(
+            bottom: mq.height * 0.15,
+            left: mq.width * 0.05,
+            width: mq.width * 0.9,
+            height: mq.height * 0.07,
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(),
+              icon: Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Image.asset(
+                  'assets/images/icon_google.png',
+                  height: mq.height * 0.05,
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+              },
+              label: RichText(
+                text: const TextSpan(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(text: "Sign In with "),
+                    TextSpan(text: "Google"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
